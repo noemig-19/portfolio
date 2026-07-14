1aeca51d625070024891c007839c31f23e5a5dd9
noemi bonilla <noemibonilla95@hotmail.com>
2026-07-14 21:38:11 +0200

    personalizazzione navbar basica

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 
 function Navbar() {
-  return (
-    <nav>
-        <li>about</li>
-        <li>skills</li>
-        <li>progetti</li>
-        <li>contatto</li>
-        <li>curriculum</li>
-    </nav>
-  )
+    return (
+
+        <nav className="fixed top-2">
+            <div className="max-w-150 m-auto flex  gap-0.2 p-4 cursor-pointer bg-mauve-900/80 text-white capitalize font-bold rounded-2xl">
+                <li>about</li>
+                <li >skills</li>
+                <li>progetti</li>
+                <li>contatto</li>
+                <li>curriculum</li>
+            </div>
+
+        </nav>
+
+    )
 }
 
 export default Navbar
