b883cc1749ea71c04fb25c94ee5c824b5e7a6b0f
noemi bonilla <noemibonilla95@hotmail.com>
2026-07-20 16:47:09 +0200

    refattorizando stile: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import { FaLinkedin } from "react-icons/fa";
 function Header() {
   return (
 
-    <header className="fixed top-4 p-2 left-2  bg-neutral-900/50  border border-neutral-700 rounded-2xl flex flex-col gap-4 max-lg:w-screen max-lg:top-0 max-lg:left-0 max-lg:pt-4 max-lg:border-b-0 max-lg:rounded-none max-[1050px]:hidden  max-lg:block">
+    <header className="fixed top-4 p-2 left-16  bg-neutral-900/50  border border-neutral-700 rounded-2xl flex flex-col gap-4 max-lg:w-screen max-lg:top-0 max-lg:left-0 max-lg:pt-4 max-lg:border-b-0 max-lg:rounded-none max-[1050px]:hidden  max-lg:block">
       <div className="flex flex-col gap-1.8 ">
         <img src="https://static.vecteezy.com/system/resources/thumbnails/050/907/439/small/3d-cartoon-woman-with-brown-hair-in-blue-shirt-png.png" alt="my-picture" className="max-w-50 max-[1186px]:hidden" />
 
